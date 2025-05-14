448e8a17641b4c1a23e102ccd8a0b928790d453f
Jaël Champagne Gareau <champagne_gareau.jael@uqam.ca>
2025-05-14 13:50:22 -0400

    support multiple mode (user, host, both)

--- README.md
+++ README.md
@@ -22,10 +22,19 @@ ya pack -a jaja360/yatline-hostname-username
 require("yatline-hostname-username"):setup()
 ```
 
+Optionnally, you can customize the color and the mode of the output:
+
+```lua
+require("yatline-hostname-username"):setup({
+  color = "silver",
+  mode = "both" -- "host", "user", "both"
+})
+```
+
 Then, add it in one of your sections in the yatline configuration using:
 
 ```lua
-{ type = "coloreds", custom = false, name = "hostname-username" }
+{ type = "coloreds", custom = false, name = "hostname_username" }
 ```
 
 ## Credits
--- main.lua
+++ main.lua
@@ -1,28 +1,39 @@
 local function setup(_, options)
-	options = options or {}
+  options = options or {}
 
-	local config = {
-		hostname_username_color = options.hostname_username_color or "silver",
-	}
+  local config = {
+    color = options.color or "silver",
+    mode = options.mode or "both",  -- "both", "user", or "host"
+  }
 
-	if Yatline ~= nil then
-		function Yatline.coloreds.get:hostname_username()
+  if Yatline ~= nil then
+    function Yatline.coloreds.get:hostname_username()
       if ya.target_family() ~= "unix" then
-				return {}
+        return {}
       end
 
-      local user = ya.user_name()
-      local host = ya.host_name()
-      if not user or not host then
-				return {}
+      local user = ya.user_name() or ""
+      local host = ya.host_name() or ""
+      local out = ""
+      if config.mode == "both" then
+        out = string.format("%s@%s", user, host)
+      elseif config.mode == "user" then
+        out = user
+      elseif config.mode == "host" then
+        out = host
+      else
+        return {}  -- invalid config value, skip display
       end
 
-			local out = string.format("%s@%s", user, host)
+      if out == "" then
+        return {}
+      end
+
       return {
-        { out, config.hostname_username_color }
+        { out, config.color }
       }
-		end
-	end
+    end
+  end
 end
 
 return { setup = setup }
